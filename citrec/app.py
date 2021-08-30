6fe0ddbcbce88391bad8c64c7b218cc7b5ecdc2c
Chenyang Li <li_chenyang@hotmail.com>
2021-08-30 17:45:20 +0200

    Add template for marking list

--- citrec/app.py
+++ citrec/app.py
@@ -2,31 +2,92 @@ from flask import Flask, render_template
 from bson.objectid import ObjectId
 import pymongo
 import datetime
-
-# from CitRec import CitRec
+from CitRec import CitRec
 
 
 app = Flask(__name__)
 server_adress = "localhost:27017"
 client = pymongo.MongoClient(server_adress)
-db = client["CitBot"]
-mark_list = db["Lists"]
-selected_items = db["Items"]
+db_citbot = client["CitBot"]
+mark_list = db_citbot["Lists"]
+selected_items = db_citbot["Items"]
+db_citrec = client["CitRec"]
+aminer = db_citrec["AMiner"]
+dblp = db_citrec["DBLP"]
+
+
+def find_papers(ids_sources):
+    finded_papers = []
+    for i in range(len(ids_sources)):
+        ids_sources[i] = tuple(ids_sources[i].split(','))
+    for i, source in ids_sources:
+        if source == "dblp":
+            try:
+                dic = dblp.find(
+                    {"_id": ObjectId(i)},
+                    {
+                        "title": 1,
+                        "author": 1,
+                        "year": 1,
+                        "booktitle": 1,
+                        "journal": 1,
+                        "ee": 1,
+                    },
+                ).next()
+            except StopIteration:
+                dic = dblp.find(
+                    {"_id": i},
+                    {
+                        "title": 1,
+                        "author": 1,
+                        "year": 1,
+                        "booktitle": 1,
+                        "journal": 1,
+                        "ee": 1,
+                    },
+                ).next()
+            dic["source"] = "dblp"
+            finded_papers.append(dic)
+        else:
+            try:
+                dic = aminer.find(
+                    {"_id": i},
+                    {
+                        "title": 1,
+                        "authors.name": 1,
+                        "venue.raw": 1,
+                        "year": 1,
+                        "url": 1,
+                    },
+                ).next()
+            except StopIteration: 
+                dic = aminer.find(
+                    {"_id": ObjectId(i)},
+                    {
+                        "title": 1,
+                        "authors.name": 1,
+                        "venue.raw": 1,
+                        "year": 1,
+                        "url": 1,
+                    },
+                ).next()
+            dic["source"] = "aminer"
+            finded_papers.append(dic)
+    return finded_papers
 
 
 @app.route("/rec/<context>")
 def rec(context):
-    # citrec = CitRec()
-    # rec_list, rec_list_ref = citrec(context)
-    file = open("rec_list.txt", "r")
-    rec_list = eval(file.read())
-    file.close()
-    # return str(rec_list) + str(rec_list_ref)
+    citrec = CitRec()
+    rec_list, rec_list_ref = citrec(context)
+    # file = open("rec_list.txt", "r")
+    # rec_list = eval(file.read())
+    # file.close()
     return render_template(
         "rec_result.json.jinja2",
         context=context,
         rec_list=rec_list[:10],
-        rec_list_ref=[],
+        rec_list_ref=rec_list_ref[:10],
     )
 
 
@@ -34,7 +95,8 @@ def rec(context):
 def actions(payload):
     msg = eval(payload)
     print(payload)
-    if isinstance(msg["msg"], type(list)):
+    # Select items in the checkbox, store the selected items
+    if isinstance(msg["msg"], list) or msg["msg"] == "[]":
         try:
             selected_items.insert_one(
                 {
@@ -54,6 +116,8 @@ def actions(payload):
                 },
             )
         return "Selected items have been stored."
+
+    # clicked the add2list button 
     elif "list" in msg["msg"]:
         try:
             mark = selected_items.find(
@@ -63,7 +127,7 @@ def actions(payload):
             return {
                 "text": "No items have been selected (or data expired due to long periods of inactivity), please select items at first 🥺"
             }
-        if mark == []:
+        if mark == "[]":
             return {
                 "text": "No items have been selected, please select items at first 🥺"
             }
@@ -82,7 +146,8 @@ def actions(payload):
                 mark_list.update(
                     {"_id": str(msg["user"])},
                     {
-                        "marked": set(mark),
+                        # delete duplicate papers
+                        "marked": sorted(set(mark),key=mark.index),
                         "expireAt": datetime.datetime.utcnow()
                         + datetime.timedelta(days=60),
                     },
@@ -90,15 +155,22 @@ def actions(payload):
             return {
                 "text": 'Selected items have been added to the marking list, send "!list" to see the marking list 😉'
             }
+    return {
+                "text": 'An error occurred 😖'
+            }
 
 
 @app.route("/lists/<payload>")
 def lists(payload):
     msg = eval(payload)
     try:
-        marked = mark_list.find({"_id": str(msg["user"])}).next()["marked"]
+        marked_ids = mark_list.find({"_id": str(msg["user"])}).next()["marked"]
     except StopIteration:
         return {
             "text": "No papers in your marking list (or data expired due to long periods of inactivity), please add items into the marking list at first 🥺"
         }
-    return 
+    marked_papers = find_papers(marked_ids)
+    return render_template(
+        "mark_list.json.jinja2",
+        marked_papers = marked_papers
+    )
