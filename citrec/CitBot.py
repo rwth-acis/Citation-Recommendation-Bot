1fef8ed9c8972ff3598cdaed118e270b2288d819
Chenyang Li <li_chenyang@hotmail.com>
2021-09-06 17:41:05 +0200

    !rec and !list are done except for delall

--- citrec/CitBot.py
+++ citrec/CitBot.py
@@ -9,7 +9,6 @@ client = pymongo.MongoClient(server_adress)
 db_citbot = client["CitBot"]
 mark_lists = db_citbot["Lists"]
 lists_temp = db_citbot["Lists_temp"]
-selected_items = db_citbot["Items"]
 results = db_citbot["Results"]
 db_citrec = client["CitRec"]
 aminer = db_citrec["AMiner"]
@@ -20,14 +19,14 @@ def generate_rec_result(context, rec_list, ref_list, user_id):
     try:
         marked_papers = mark_lists.find({"_id": user_id}).next()["marked"]
         for paper in rec_list:
-            id_source = str(paper["_id"]) + ',' + paper["source"]
+            id_source = str(paper["_id"]) + "," + paper["source"]
             if id_source in marked_papers:
                 paper["inList"] = True
             else:
                 paper["inList"] = False
         if ref_list:
             for paper in ref_list:
-                id_source = str(paper["_id"]) + ',' + paper["source"]
+                id_source = str(paper["_id"]) + "," + paper["source"]
                 if id_source in marked_papers:
                     paper["inList"] = True
                 else:
@@ -89,32 +88,34 @@ def generate_rec_result(context, rec_list, ref_list, user_id):
             )
         }
 
+
 def flip_page_rec(value, time):
     rec_list_id, page = tuple(value.split(","))
     page = int(page)
     try:
         rec_list = results.find({"_id": ObjectId(rec_list_id)}).next()
         return {
-        "blocks": render_template(
-            "rec_result.json.jinja2",
-            context=rec_list["context"],
-            rec_list=rec_list["papers"][(page * 5) : (page * 5 + 5)],
-            rec_list_id=rec_list["_id"],
-            ref_list_id=rec_list["refId"],
-            page=page,
-        ),
-        "updateBlock": "true",
-        "ts": time,
-    }
+            "blocks": render_template(
+                "rec_result.json.jinja2",
+                context=rec_list["context"],
+                rec_list=rec_list["papers"][(page * 5) : (page * 5 + 5)],
+                rec_list_id=rec_list["_id"],
+                ref_list_id=rec_list["refId"],
+                page=page,
+            ),
+            "updateBlock": "true",
+            "ts": time,
+        }
     except StopIteration:
         return {
-            "text": "This is an outdated message (more then 60 minutes), please send me the citation context again 🥺"
+            "text": "This is an outdated message (more than 60 minutes), please send me the citation context again 🥺"
         }
 
+
 def show_classic_papers(ref_list_id):
-        try:
-            ref_list = results.find({"_id": ObjectId(ref_list_id)}).next()
-            return {
+    try:
+        ref_list = results.find({"_id": ObjectId(ref_list_id)}).next()
+        return {
             "blocks": render_template(
                 "ref_result.json.jinja2",
                 context=ref_list["context"],
@@ -124,10 +125,11 @@ def show_classic_papers(ref_list_id):
                 page=0,
             ),
         }
-        except StopIteration:
-            return {
-                "text": "This is an outdated message (more then 60 minutes), please send me the citation context again 🥺"
-            }
+    except StopIteration:
+        return {
+            "text": "This is an outdated message (more than 60 minutes), please send me the citation context again 🥺"
+        }
+
 
 def flip_page_ref(value, time):
     ref_list_id, page = tuple(value.split(","))
@@ -135,33 +137,33 @@ def flip_page_ref(value, time):
     try:
         ref_list = results.find({"_id": ObjectId(ref_list_id)}).next()
         return {
-        "blocks": render_template(
-            "ref_result.json.jinja2",
-            context=ref_list["context"],
-            ref_list=ref_list["papers"][(page * 5) : (page * 5 + 5)],
-            ref_list_id=ref_list["_id"],
-            next_page=True if len(ref_list[(page * 5):]) > 5 else False,
-            page=page,
-        ),
-        "updateBlock": "true",
-        "ts": time,
-    }
+            "blocks": render_template(
+                "ref_result.json.jinja2",
+                context=ref_list["context"],
+                ref_list=ref_list["papers"][(page * 5) : (page * 5 + 5)],
+                ref_list_id=ref_list["_id"],
+                next_page=True if len(ref_list["papers"][(page * 5) :]) > 5 else False,
+                page=page,
+            ),
+            "updateBlock": "true",
+            "ts": time,
+        }
     except StopIteration:
         return {
-            "text": "This is an outdated message (more then 60 minutes), please send me the citation context again 🥺"
+            "text": "This is an outdated message (more than 60 minutes), please send me the citation context again 🥺"
         }
+
 
 def add_to_list(value, time, user_id):
     rec_or_ref, ind, page, paper_id, paper_source = tuple(value.split(","))
     page = int(page)
-    mark = [paper_id + ',' + paper_source]
+    mark = [paper_id + "," + paper_source]
     try:
         mark_lists.insert_one(
             {
                 "_id": user_id,
                 "marked": mark,
-                "expireAt": datetime.datetime.utcnow()
-                + datetime.timedelta(days=60),
+                "expireAt": datetime.datetime.utcnow() + datetime.timedelta(days=60),
             }
         )
     except pymongo.errors.DuplicateKeyError:
@@ -171,47 +173,51 @@ def add_to_list(value, time, user_id):
             {
                 # delete duplicate papers
                 "marked": sorted(set(mark), key=mark.index),
-                "expireAt": datetime.datetime.utcnow()
-                + datetime.timedelta(days=60),
+                "expireAt": datetime.datetime.utcnow() + datetime.timedelta(days=60),
             },
         )
 
     try:
         rec_or_ref_result = results.find({"_id": ObjectId(ind)}).next()
-        for paper in (rec_or_ref_result["papers"])[(page * 5):]:
+        for paper in (rec_or_ref_result["papers"])[(page * 5) :]:
             if paper["_id"] == paper_id or paper["_id"] == ObjectId(paper_id):
                 paper["inList"] = True
-        results.update_one({"_id": ObjectId(ind)}, {"$set": {'papers': rec_or_ref_result["papers"]}})
+        results.update_one(
+            {"_id": ObjectId(ind)}, {"$set": {"papers": rec_or_ref_result["papers"]}}
+        )
         if rec_or_ref == "rec":
             return {
-        "blocks": render_template(
-            "rec_result.json.jinja2",
-            context=rec_or_ref_result["context"],
-            rec_list=rec_or_ref_result["papers"][(page * 5) : (page * 5 + 5)],
-            rec_list_id=rec_or_ref_result["_id"],
-            ref_list_id=rec_or_ref_result["refId"],
-            page=page,
-        ),
-        "updateBlock": "true",
-        "ts": time,
-    }
+                "blocks": render_template(
+                    "rec_result.json.jinja2",
+                    context=rec_or_ref_result["context"],
+                    rec_list=rec_or_ref_result["papers"][(page * 5) : (page * 5 + 5)],
+                    rec_list_id=rec_or_ref_result["_id"],
+                    ref_list_id=rec_or_ref_result["refId"],
+                    page=page,
+                ),
+                "updateBlock": "true",
+                "ts": time,
+            }
         else:
             return {
-        "blocks": render_template(
-            "ref_result.json.jinja2",
-            context=rec_or_ref_result["context"],
-            ref_list=rec_or_ref_result["papers"][(page * 5) : (page * 5 + 5)],
-            ref_list_id=rec_or_ref_result["_id"],
-            next_page=True if len(rec_or_ref_result["papers"][(page * 5):]) > 5 else False,
-            page=page,
-        ),
-        "updateBlock": "true",
-        "ts": time,
-    }
+                "blocks": render_template(
+                    "ref_result.json.jinja2",
+                    context=rec_or_ref_result["context"],
+                    ref_list=rec_or_ref_result["papers"][(page * 5) : (page * 5 + 5)],
+                    ref_list_id=rec_or_ref_result["_id"],
+                    next_page=True
+                    if len(rec_or_ref_result["papers"][(page * 5) :]) > 5
+                    else False,
+                    page=page,
+                ),
+                "updateBlock": "true",
+                "ts": time,
+            }
     except StopIteration:
         return {
-            "text": "I have add this paper to the marking list, send \"!list\" to see the marking list 😉\n However, this message is outdated, so I could not update the message 🥺"
+            "text": 'I have added this paper to the marking list, send "!list" to see the marking list 😉\n However, this message is outdated, so I could not update the message 🥺'
         }
+
 
 def find_papers_in_list(user_id):
     try:
@@ -275,12 +281,51 @@ def find_papers_in_list(user_id):
             dic["source"] = "aminer"
             finded_papers.append(dic)
     list_id = ObjectId()
-    lists_temp.insert_one({"_id": list_id, "papers": finded_papers, "expireAt": datetime.datetime.utcnow()
-                + datetime.timedelta(minutes=20),})
+    lists_temp.insert_one(
+        {
+            "_id": list_id,
+            "papers": finded_papers,
+            "expireAt": datetime.datetime.utcnow() + datetime.timedelta(minutes=20),
+        }
+    )
     return list_id, finded_papers
 
+
+def flip_page_list(value, time, user_id):
+    list_id, page = tuple(value.split(","))
+    page = int(page)
+    try:
+        marked_papers = lists_temp.find({"_id": ObjectId(list_id)}).next()["papers"]
+        return {
+            "blocks": render_template(
+                "mark_list.json.jinja2",
+                list_id=list_id,
+                marked_papers=marked_papers[(page * 5) : (page * 5 + 5)],
+                page=page,
+                next_page=True if len(marked_papers[(page * 5) :]) > 5 else False,
+            ),
+            "updateBlock": "true",
+            "ts": time,
+        }
+    except StopIteration:
+        # no temp, find papers in list again
+        list_id, marked_papers = find_papers_in_list(user_id)
+        return {
+            "blocks": render_template(
+                "mark_list.json.jinja2",
+                list_id=list_id,
+                marked_papers=marked_papers[(page * 5) : (page * 5 + 5)],
+                page=page,
+                next_page=True if len(marked_papers[(page * 5) :]) > 5 else False,
+            ),
+            "updateBlock": "true",
+            "ts": time,
+        }
+
+
 def del_paper_in_list(value, time, user_id):
-    list_id, paper_id, paper_source = tuple(value.split(","))
+    list_id, paper_id, paper_source, page = tuple(value.split(","))
+    page = int(page)
 
     try:
         marked_papers = mark_lists.find({"_id": user_id}).next()["marked"]
@@ -292,27 +337,38 @@ def del_paper_in_list(value, time, user_id):
     try:
         # delete the papers id in Lists
         marked_papers.remove(paper_id + "," + paper_source)
-        mark_lists.update_one({"_id": user_id}, {"$set": {'marked': marked_papers}})
+        mark_lists.update_one({"_id": user_id}, {"$set": {"marked": marked_papers}})
         # delete from the temp
-        list_papers = lists_temp.find({"_id": ObjectId(list_id)}).next()["papers"]
-        for paper in list_papers:
+        marked_papers = lists_temp.find({"_id": ObjectId(list_id)}).next()["papers"]
+        for paper in marked_papers:
             if paper["_id"] == paper_id or paper["_id"] == ObjectId(paper_id):
-                list_papers.remove(paper)
+                marked_papers.remove(paper)
                 break
-        lists_temp.update({"_id": ObjectId(list_id)}, {"$set": {'papers': list_papers}})
+        lists_temp.update(
+            {"_id": ObjectId(list_id)}, {"$set": {"papers": marked_papers}}
+        )
         return {
-        "blocks": render_template("mark_list.json.jinja2", list_id=list_id, marked_papers=list_papers),
-        "updateBlock": "true",
-        "ts": time,
-    }
+            "blocks": render_template(
+                "mark_list.json.jinja2",
+                list_id=list_id,
+                marked_papers=marked_papers[(page * 5) : (page * 5 + 5)],
+                page=page,
+                next_page=True if len(marked_papers[(page * 5) :]) > 5 else False,
+            ),
+            "updateBlock": "true",
+            "ts": time,
+        }
     # no temp, find papers in list again
     except (StopIteration, ValueError):
-        list_id, list_papers = find_papers_in_list(user_id)
+        list_id, marked_papers = find_papers_in_list(user_id)
         return {
-        "blocks": render_template("mark_list.json.jinja2", list_id=list_id, marked_papers=list_papers),
-        "updateBlock": "true",
-        "ts": time,
-    }
-    
-def flip_page_list(value, time, user_id):
-    pass
+            "blocks": render_template(
+                "mark_list.json.jinja2",
+                list_id=list_id,
+                marked_papers=marked_papers[(page * 5) : (page * 5 + 5)],
+                page=page,
+                next_page=True if len(marked_papers[(page * 5) :]) > 5 else False,
+            ),
+            "updateBlock": "true",
+            "ts": time,
+        }
--- citrec/app.py
+++ citrec/app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template
 from bson.objectid import ObjectId
 import pymongo
 import datetime
-# from CitRec import CitRec
+
+from CitRec import CitRec
 import CitBot
 
 app = Flask(__name__)
@@ -10,19 +11,18 @@ server_adress = "localhost:27017"
 client = pymongo.MongoClient(server_adress)
 db_citbot = client["CitBot"]
 mark_lists = db_citbot["Lists"]
-selected_items = db_citbot["Items"]
 results = db_citbot["Results"]
 db_citrec = client["CitRec"]
 aminer = db_citrec["AMiner"]
 dblp = db_citrec["DBLP"]
 
 
-# @app.route("/rec/<payload>")
-# def rec(payload):
-#     payload = eval(payload)
-#     citrec = CitRec()
-#     rec_list, ref_list = citrec(payload["context"])
-#     return CitBot.generate_rec_result(context=payload["context"], rec_list=rec_list, ref_list=ref_list, user_id=payload["user"])
+@app.route("/rec/<payload>")
+def rec(payload):
+    payload = eval(payload)
+    citrec = CitRec()
+    rec_list, ref_list = citrec(payload["context"])
+    return CitBot.generate_rec_result(context=payload["context"], rec_list=rec_list, ref_list=ref_list, user_id=payload["user"])
 
 
 @app.route("/actions/<payload>")
@@ -40,15 +40,34 @@ def actions(payload):
         return CitBot.show_classic_papers(ref_list_id=actionInfo["value"])
 
     # when clicking previous page and next page in classic paper list
-    elif actionInfo["actionId"] == "previous_ref" or actionInfo["actionId"] == "next_ref":
+    elif (
+        actionInfo["actionId"] == "previous_ref" or actionInfo["actionId"] == "next_ref"
+    ):
         return CitBot.flip_page_ref(value=actionInfo["value"], time=payload["time"])
 
     # when clicking the add2list button
     elif actionInfo["actionId"] == "add2list":
-        return CitBot.add_to_list(value=actionInfo["value"], time=payload["time"], user_id=payload["user"])
-        
+        return CitBot.add_to_list(
+            value=actionInfo["value"], time=payload["time"], user_id=payload["user"]
+        )
+
+    # when clicking the delete button
     elif actionInfo["actionId"] == "del":
-        return CitBot.del_paper_in_list(value=actionInfo["value"], time=payload["time"], user_id=payload["user"])
+        return CitBot.del_paper_in_list(
+            value=actionInfo["value"], time=payload["time"], user_id=payload["user"]
+        )
+
+    # when clicking previous page and next page in marking list
+    elif (
+        actionInfo["actionId"] == "next_list"
+        or actionInfo["actionId"] == "previous_list"
+    ):
+        return CitBot.flip_page_list(
+            value=actionInfo["value"], time=payload["time"], user_id=payload["user"]
+        )
+    
+    elif actionInfo["actionId"] == "delall":
+        return 
 
     return {"text": "An error occurred 😖"}
 
@@ -58,10 +77,18 @@ def lists(payload):
     payload = eval(payload)
     user_id = str(payload["user"])
     list_id, marked_papers = CitBot.find_papers_in_list(user_id)
+    print(len(marked_papers))
     if not list_id:
         return {
             "text": "No papers in your marking list (or data expired due to long periods of inactivity), please add items into the marking list at first 🥺"
         }
     else:
-        return {"blocks": render_template("mark_list.json.jinja2", list_id=list_id, marked_papers=marked_papers)
+        return {
+            "blocks": render_template(
+                "mark_list.json.jinja2",
+                list_id=list_id,
+                marked_papers=marked_papers[:5],
+                page=0,
+                next_page=True if len(marked_papers) > 5 else False,
+            )
         }
